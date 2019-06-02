c8fb2fb05689320efd46a5d07f9bea4bdaf9607e
shlee <navystyle@naver.com>
2019-06-02 22:48:01 +0900

    .

--- api-test/variables.js
+++
@@ -1,5 +0,0 @@
-const IS_OFFLINE = process.env.IS_OFFLINE;
-const BASE_DOMAIN = 'https://gi2u7bdmtk.execute-api.ap-northeast-2.amazonaws.com/dev';
-const BASE_DOMAIN_LOCAL = 'http://localhost:3000';
-
-module.exports = {IS_OFFLINE, BASE_DOMAIN, BASE_DOMAIN_LOCAL};
